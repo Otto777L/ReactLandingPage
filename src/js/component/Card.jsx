799c24ec7b41083178918e9facf90001a99ce091
Otto777L <otto.lh12@gmail.com>
2023-02-07 01:21:37 +0000

    Modified last changes to Cards, Top and Navbar

---
+++ src/js/component/Card.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+function Card() {
+    return (<div className="container">
+        <div class="d-flex justify-content-center row row-cols-1 row-cols-md-4 g-4 mb-3">
+  <div class="col">
+    <div class="card h-100 text-center">
+      <img src="https://picsum.photos/350/150" class="cardimg card-img-top" alt="..."></img>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Libero justo laoreet sit amet cursus sit amet dictum. Scelerisque fermentum dui faucibus in ornare quam.</p>
+      </div>
+        <div class="card-footer">
+            <a href="#" class="btn btn-primary">Find out More!</a>
+        </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100 text-center">
+      <img src="https://picsum.photos/350/150" class="cardimg card-img-top" alt="..."></img>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </div>
+        <div class="card-footer">
+            <a href="#" class="btn btn-primary">Find out More!</a>
+        </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100 text-center">
+      <img src="https://picsum.photos/350/150" class="cardimg card-img-top" alt="..."></img>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Libero justo laoreet sit amet cursus sit amet dictum. Scelerisque fermentum dui faucibus in ornare quam.</p>
+      </div>
+        <div class="card-footer">
+            <a href="#" class="btn btn-primary">Find out More!</a>
+        </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100 text-center">
+      <img src="https://picsum.photos/350/150" class="cardimg card-img-top" alt="..."></img>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </div>
+        <div class="card-footer">
+            <a href="#" class="btn btn-primary">Find out More!</a>
+        </div>
+    </div>
+  </div>
+</div>
+</div>
+    )
+}
+
+export default Card;
